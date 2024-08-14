3a5a945cf1c088fe47ffedbeade174fe2cdfa892
dixit5302 <mugithedeekshith@gmail.com>
2024-08-14 15:08:44 +0530

    file deleted

--- practice.js
+++
@@ -1,8 +0,0 @@
-async function getData() {
-    let URL = 'https://dummyjson.com/products'
-    let user = await fetch(URL)
-    let response = await user.json()
-    console.log(response);
-    
-}
-getData()
